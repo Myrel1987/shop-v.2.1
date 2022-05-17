99a1d94f6fadccf7e8aa2fbfac3457117751569a
Apetrei <myrel1987@gmail.com>
2022-05-17 23:02:03 +0200

    updated to redux.2

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
-<<<<<<< HEAD
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App";
-import { UserProvider } from "./contexts/user.context";
-import { CategoriesProvider } from "./contexts/categories.context";
-import { CartProvider } from "./contexts/cart.context";
-=======
-import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-
-import App from './App';
-import { store, persistor } from './store/store';
->>>>>>> lesson-38
+import { store, persistor } from "./store/store";
 
 import "./index.scss";
 
