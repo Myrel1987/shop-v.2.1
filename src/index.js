6b33eb7a3b8f70f47209577a898df8c25134b572
Apetrei <myrel1987@gmail.com>
2022-05-18 22:35:18 +0200

    added libraries for stripe

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import { Elements } from "@stripe/react-stripe-js";
 
 import App from "./App";
 import { store, persistor } from "./store/store";
